d8a47d5194dd8747f30f6b33c11c95ed04ec679c
Ograll <stefplotva@gmail.com>
2022-10-12 01:45:42 +0200

    Old functions was transferred to garbage folder.

--- CamSimWindow.xaml.cs
+++ CamSimWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Data.Common;
 using System.Windows;
-using MySql.Data.MySqlClient;
 
 namespace AthenaNewGeneration;
 
@@ -18,39 +16,5 @@ public partial class CamSimWindow
         Random randomizer = new();
         var ourDonate = (short)randomizer.Next(1, 500);
         OurDonatShowing.Text = ourDonate.ToString();
-        var conn = DbUtils.GetDbConnection();
-        conn.Open();
-        try
-        {
-            QueryDonatesList(conn);
-        }
-        catch (Exception exception)
-        {
-            MessageBox.Show($"Error: {exception.Message}/n{exception.StackTrace}");
-        }
-        finally
-        {
-            conn.Close();
-            conn.Dispose();
-        }
-    }
-
-    private static void QueryDonatesList(MySqlConnection connection)
-    {
-        const string query = "SELECT * FROM DonatesPrises";
-        MySqlCommand command = new();
-        command.Connection = connection;
-        command.CommandText = query;
-        using(DbDataReader reader = command.ExecuteReader())
-        {
-            if(reader.HasRows)
-            {
-                while(reader.Read())
-                {
-                    var value = reader.GetString(1);
-                    MessageBox.Show(value);
-                }
-            }
-        }
     }
 }
